217420e815a23dfc19a406133c899f3d1e29ff05
sadunuthula <sadunuthula@ebay.com>
2019-10-02 15:37:35 -0700

    Initial Push for Bookstore

---
+++ src/main/java/com/example/bookstore/controller/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.bookstore.controller;
+
+public class BookNotFoundException  extends RuntimeException{
+    public BookNotFoundException(Long id) {
+        super("Book id not found : " + id);
+    }
+}
---
+++ src/main/java/com/example/bookstore/model/BookRepository.java
@@ -0,0 +1,11 @@
+package com.example.bookstore.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByName(String name);
+
+}
